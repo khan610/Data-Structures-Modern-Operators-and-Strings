886fd8280e833ce1ee782a0f087ce0c750422d51
Andrea Trajkovic <trajkovicandrea@gmail.com>
2022-05-02 17:02:09 +0200

    Sets practice

--- script.js
+++ script.js
@@ -51,38 +51,60 @@ const restaurant = {
   },
 };
 
-const ordersSet = new Set([
-  'Pasta',
-  'Pizza',
-  'Pizza',
-  'Risotto',
-  'Pasta',
-  'Pizza',
-]);
+const rest = new Map();
+rest, set('name', 'Classico Italiano');
+rest.set(1, 'Firenze, Italy');
+console.log(rest.set(2, 'Lisbon, Portugal'));
 
-console.log(ordersSet);
+rest
+  .set('categoriez', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open :D')
+  .set(false, 'We are closed :(');
 
-console.log(new Set('Jonas'));
+console.log(rest.get('name'));
+console.log(rest.get(true));
+console.log(rest.get(1));
 
-console.log(ordersSet.size);
-console.log(ordersSet.has('Pizza'));
-console.log(ordersSet.has('Bread'));
-ordersSet.add('Garlic Bread');
-ordersSet.add('Garlic Bread');
-ordersSet.delete('Risotto');
-// ordersSet.clear();
+const time = 21;
+console.log(rest.get(time > rest.get('open' && time < rest.get('close'))));
 
-console.log(ordersSet);
+////////////////////////////////////////////////////////////////////
+//          Sets
+//
+// const ordersSet = new Set([
+//   'Pasta',
+//   'Pizza',
+//   'Pizza',
+//   'Risotto',
+//   'Pasta',
+//   'Pizza',
+// ]);
 
-for (const order of ordersSet) console.log(order);
+// console.log(ordersSet);
 
-// Example
-const staff = ['Waiter', 'Chef', 'Waiter', 'Menager', 'Chef', 'Waiter'];
-const staffUnique = [...new Set(staff)];
-console.log(staffUnique);
-console.log(new Set(['Waiter', 'Chef', 'Waiter', 'Menager', 'Chef', 'Waiter']));
+// console.log(new Set('Jonas'));
 
-console.log(new Set('jonasschmedtmann').size);
+// console.log(ordersSet.size);
+// console.log(ordersSet.has('Pizza'));
+// console.log(ordersSet.has('Bread'));
+// ordersSet.add('Garlic Bread');
+// ordersSet.add('Garlic Bread');
+// ordersSet.delete('Risotto');
+// // ordersSet.clear();
+
+// console.log(ordersSet);
+
+// for (const order of ordersSet) console.log(order);
+
+// // Example
+// const staff = ['Waiter', 'Chef', 'Waiter', 'Menager', 'Chef', 'Waiter'];
+// const staffUnique = [...new Set(staff)];
+// console.log(staffUnique);
+// console.log(new Set(['Waiter', 'Chef', 'Waiter', 'Menager', 'Chef', 'Waiter']));
+
+// console.log(new Set('jonasschmedtmann').size);
 
 // const game = {
 //   team1: 'Bayern Munich',
